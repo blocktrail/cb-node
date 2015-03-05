dfc19e2b72cb859aef6fee71ca7d7d747e765e53
Daniel Cousens <github@dcousens.com>
2015-03-05 12:29:50 +1100

    index: merge all modules into one

--- src/index.js
+++ src/index.js
@@ -1,19 +1,86 @@
-var Addresses = require('./addresses')
-var Blocks = require('./blocks')
-var Transactions = require('./transactions')
+var httpify = require('httpify')
+var jsend = require('jsend')
 
-function Node(baseURL, network) {
-  this.addresses = new Addresses(baseURL + '/addresses', 10000)
-  this.blocks = new Blocks(baseURL + '/blocks', 10000)
-  this.transactions = new Transactions(baseURL + '/transactions', 10000)
+function req (url, body, plural, timeout, callback) {
+  httpify({
+    method: 'POST',
+    url: url,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(body),
+    timeout: timeout
+  }, function (err, res) {
+    if (err) return callback(err)
+    if (res.statusCode !== 200) return callback(new Error('Error ' + res.statusCode))
 
-  this.network = network || 'bitcoin'
+    var body = res.body
+    if (!jsend.isValid(body)) return callback(new Error('Invalid JSend response'))
+    if (body.status === 'fail' || body.status === 'error') {
+      return callback(new Error(body.message || body.data))
+    }
+
+    return callback(undefined, plural ? body.data : body.data[0])
+  })
 }
 
-Node.Addresses = Addresses
-Node.Blocks = Blocks
-Node.Transactions = Transactions
+function Node (base) {
+  var self = this
 
-Node.prototype.getNetwork = function() { return this.network }
+  this.addresses = {
+    summary: function (addresses, callback) {
+      req(base + '/addresses/summary', { addresses: [].concat(addresses) }, Array.isArray(addresses), self.timeout, callback)
+    },
+
+    transactions: function (addresses, blockHeight, callback) {
+      // optional blockHeight
+      if ('function' === typeof blockHeight) {
+        callback = blockHeight
+        blockHeight = 0
+      }
+
+      req(base + '/addresses/transactions', { addresses: [].concat(addresses), blockHeight: blockHeight, }, true, self.timeout, callback)
+    },
+
+    unspents: function (addresses, callback) {
+      req(base + '/addresses/unspents', { addresses: [].concat(addresses) }, true, self.timeout, callback)
+    }
+  }
+
+  this.blocks = {
+    get: function (blockIds, callback) {
+      req(base + '/blocks/get', { blockIds: [].concat(blockIds) }, Array.isArray(blockIds), self.timeout, callback)
+    },
+
+    latest: function (callback) {
+      req(base + '/blocks/latest', {}, true, self.timeout, callback)
+    },
+
+    propagate: function (blockIds, callback) {
+      req(base + '/blocks/propagate', { blockIds: [].concat(blockIds) }, Array.isArray(blockIds), self.timeout, callback)
+    },
+
+    summary: function (blockIds, callback) {
+      req(base + '/blocks/summary', { blockIds: [].concat(blockIds) }, Array.isArray(blockIds), self.timeout, callback)
+    }
+  }
+
+  this.timeout = 10000
+  this.transactions = {
+    get: function (txIds, callback) {
+      req(base + '/transactions/get', { txIds: [].concat(txIds) }, Array.isArray(txIds), self.timeout, callback)
+    },
+
+    latest: function (callback) {
+      req(base + '/transactions/latest', {}, true, self.timeout, callback)
+    },
+
+    propagate: function (txHexs, callback) {
+      req(base + '/transactions/propagate', { txHexs: [].concat(txHexs) }, Array.isArray(txHexs), self.timeout, callback)
+    },
+
+    summary: function (txIds, callback) {
+      req(base + '/transactions/summary', { txIds: [].concat(txIds) }, Array.isArray(txIds), self.timeout, callback)
+    }
+  }
+}
 
 module.exports = Node
--- test/index.js
+++ test/index.js
@@ -1,32 +1,12 @@
-var assert = require('assert')
 var Blockchain = require('../src/index')
-
 var url = 'https://common-blockchain.herokuapp.com'
 
-describe('Blockchain', function() {
-  describe('Constructor', function() {
-    it('defaults to the bitcoin network', function() {
-      var blockchain = new Blockchain(url)
+describe('cb-tests', function () {
+  var options = {}
 
-      assert.equal(blockchain.getNetwork(), 'bitcoin')
-    })
+  beforeEach(function () {
+    options.blockchain = new Blockchain(url)
   })
-
-  describe('getNetwork', function() {
-    it('returns the underlying network name', function() {
-      var blockchain = new Blockchain(url, 'testnet')
-
-      assert.equal(blockchain.getNetwork(), 'testnet')
-    })
-  })
-})
-
-describe('cb-tests', function() {
-	var options = {}
-
-	beforeEach(function() {
-		options.blockchain = new Blockchain(url, 'testnet')
-	})
 
 	require('cb-tester')(options)
 })
