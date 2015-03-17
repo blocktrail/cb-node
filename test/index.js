69e967071c2baa02938a06669e4513366955d2d3
Daniel Cousens <github@dcousens.com>
2015-03-18 09:40:10 +1100

    tests: add missing /v1

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 var Blockchain = require('../src/index')
-var url = 'https://common-blockchain-test.herokuapp.com'
+var url = 'https://common-blockchain-test.herokuapp.com/v1'
 
 describe('cb-tests', function () {
   var options = {}
