8b394a5af0cfacb24346b00c0c846b8907069c40
Daniel Cousens <github@dcousens.com>
2015-04-04 00:53:30 +1100

    index: simpler use of closures for less duplication

--- index.js
+++ index.js
@@ -2,42 +2,40 @@ var httpify = require('httpify')
 var jsend = require('jsend')
 var qs = require('querystring')
 
-function req (url, body, xhrOptions, callback, deconstruct) {
-  var options = {
-    method: 'POST',
-    url: url,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(body)
-  }
+function Node (base, queryParams) {
+  var queryString = queryParams ? ('?' + qs(queryParams)) : ''
+  var self = this
 
-  // extend the options object if necessary
-  for (var option in xhrOptions) {
-    options[option] = xhrOptions[option]
-  }
-
-  httpify(options, function (err, res) {
-    if (err) return callback(err)
-    if (res.statusCode !== 200) return callback(new Error('Error ' + res.statusCode))
-
-    var body = res.body
-    if (!jsend.isValid(body)) return callback(new Error('Invalid JSend response'))
-    if (body.status === 'fail' || body.status === 'error') {
-      return callback(new Error(body.message || body.data))
+  function req (url, body, callback, deconstruct) {
+    var options = {
+      method: 'POST',
+      url: base + url + queryString,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body)
     }
 
-    return callback(undefined, deconstruct ? body.data[0] : body.data)
-  })
-}
+    // extend the options object if necessary
+    for (var option in self.xhrOptions) {
+      options[option] = self.xhrOptions[option]
+    }
 
-function Node (base, defaultParams) {
-  var params = defaultParams ? ('?' + qs(defaultParams)) : ''
-  var self = this
+    httpify(options, function (err, res) {
+      if (err) return callback(err)
+      if (res.statusCode !== 200) return callback(new Error('Error ' + res.statusCode))
+
+      var body = res.body
+      if (!jsend.isValid(body)) return callback(new Error('Invalid JSend response'))
+      if (body.status === 'fail' || body.status === 'error') {
+        return callback(new Error(body.message || body.data))
+      }
+
+      return callback(undefined, deconstruct ? body.data[0] : body.data)
+    })
+  }
 
   this.addresses = {
     summary: function (addresses, callback) {
-      req(base + '/addresses/summary' + params, {
-        addresses: [].concat(addresses)
-      }, self.xhrOptions, callback, !Array.isArray(addresses))
+      req('/addresses/summary', { addresses: [].concat(addresses) }, callback, !Array.isArray(addresses))
     },
 
     transactions: function (addresses, blockHeight, callback) {
@@ -47,64 +45,47 @@ function Node (base, defaultParams) {
         blockHeight = 0
       }
 
-      req(base + '/addresses/transactions' + params, {
-        addresses: [].concat(addresses),
-        blockHeight: blockHeight
-      }, self.xhrOptions, callback)
+      req('/addresses/transactions', { addresses: [].concat(addresses), blockHeight: blockHeight }, callback)
     },
 
     unspents: function (addresses, callback) {
-      req(base + '/addresses/unspents' + params, {
-        addresses: [].concat(addresses)
-      }, self.xhrOptions, callback)
+      req('/addresses/unspents', { addresses: [].concat(addresses) }, callback)
     }
   }
 
   this.blocks = {
     get: function (blockIds, callback) {
-      req(base + '/blocks/get' + params, {
-        blockIds: [].concat(blockIds)
-      }, self.xhrOptions, callback, !Array.isArray(blockIds))
+      req('/blocks/get', { blockIds: [].concat(blockIds) }, callback, !Array.isArray(blockIds))
     },
 
     latest: function (callback) {
-      req(base + '/blocks/latest' + params, {}, self.xhrOptions, callback)
+      req('/blocks/latest', {}, callback)
     },
 
     propagate: function (blockHex, callback) {
-      req(base + '/blocks/propagate' + params, {
-        blockHex: blockHex
-      }, self.xhrOptions, callback)
+      req('/blocks/propagate', { blockHex: blockHex }, callback)
     },
 
     summary: function (blockIds, callback) {
-      req(base + '/blocks/summary' + params, {
-        blockIds: [].concat(blockIds)
-      }, self.xhrOptions, callback, !Array.isArray(blockIds))
+      req('/blocks/summary', { blockIds: [].concat(blockIds) }, callback, !Array.isArray(blockIds))
     }
   }
 
   this.transactions = {
     get: function (txIds, callback) {
-      req(base + '/transactions/get' + params, {
-        txIds: [].concat(txIds)
-      }, self.xhrOptions, callback, !Array.isArray(txIds))
+      req('/transactions/get', { txIds: [].concat(txIds) }, callback, !Array.isArray(txIds))
     },
 
     latest: function (callback) {
-      req(base + '/transactions/latest' + params, {}, self.xhrOptions, callback)
+      req('/transactions/latest', {}, callback)
     },
 
     propagate: function (txHex, callback) {
-      req(base + '/transactions/propagate' + params, {
-        txHexs: [].concat(txHex)
-      }, self.xhrOptions, callback)
+      req('/transactions/propagate', { txHexs: [].concat(txHex) }, callback)
     },
 
     summary: function (txIds, callback) {
-      req(base + '/transactions/summary' + params, {
-        txIds: [].concat(txIds)
-      }, self.xhrOptions, callback, !Array.isArray(txIds))
+      req('/transactions/summary', { txIds: [].concat(txIds) }, callback, !Array.isArray(txIds))
     }
   }
 
